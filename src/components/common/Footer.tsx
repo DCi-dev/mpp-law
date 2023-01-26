d40b8af3c63953627fa24565aa2fcbd2c7089767
CDi <ilie.dancrist@protonmail.com>
2023-01-26 15:51:52 +0200

    add contact page

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -19,7 +19,7 @@ const navigation = {
       icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 16 16" {...props}>
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
           ></path>
         </svg>
@@ -57,7 +57,7 @@ const navigation = {
   social: [
     {
       name: "Facebook",
-      href: "#",
+      href: "https://www.facebook.com/mpp.law.ro",
       icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -70,7 +70,7 @@ const navigation = {
     },
     {
       name: "LinedIn",
-      href: "#",
+      href: "https://www.facebook.com/mpp.law.ro",
       icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
@@ -115,9 +115,9 @@ export default function Footer() {
                 width={200}
                 height={50}
               />
-              <p className="text-gray-500 text-base">
-                "Soluții juridice adaptate nevoilor fiecărui client, cu
-                profesionalism și integritate."
+              <p className="text-gray-500 text-base italic">
+                Soluții juridice adaptate nevoilor fiecărui client, cu
+                profesionalism și integritate.
               </p>
 
               <div className="flex space-x-6">
---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,336 @@
+import {
+  EnvelopeIcon,
+  MapPinIcon,
+  PhoneIcon,
+} from "@heroicons/react/24/outline";
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Contact: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>MPP-Law - Contact</title>
+        <meta name="description" content="MPP Law firm" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="relative mx-auto mt-16  bg-white  sm:mt-14 ">
+        <h2 className="sr-only">Contact us</h2>
+
+        <div className="grid grid-cols-1 lg:grid-cols-3">
+          {/* Contact information */}
+          <div className="relative overflow-hidden bg-primary-700 py-10 px-6 sm:px-10 xl:p-12">
+            <div
+              className="pointer-events-none absolute inset-0 sm:hidden"
+              aria-hidden="true"
+            >
+              <svg
+                className="absolute inset-0 h-full w-full"
+                width={343}
+                height={388}
+                viewBox="0 0 343 388"
+                fill="none"
+                preserveAspectRatio="xMidYMid slice"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M-99 461.107L608.107-246l707.103 707.107-707.103 707.103L-99 461.107z"
+                  fill="url(#linear1)"
+                  fillOpacity=".1"
+                />
+                <defs>
+                  <linearGradient
+                    id="linear1"
+                    x1="254.553"
+                    y1="107.554"
+                    x2="961.66"
+                    y2="814.66"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stopColor="#fff" />
+                    <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                  </linearGradient>
+                </defs>
+              </svg>
+            </div>
+            <div
+              className="pointer-events-none absolute top-0 right-0 bottom-0 hidden w-1/2 sm:block lg:hidden"
+              aria-hidden="true"
+            >
+              <svg
+                className="absolute inset-0 h-full w-full"
+                width={359}
+                height={339}
+                viewBox="0 0 359 339"
+                fill="none"
+                preserveAspectRatio="xMidYMid slice"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M-161 382.107L546.107-325l707.103 707.107-707.103 707.103L-161 382.107z"
+                  fill="url(#linear2)"
+                  fillOpacity=".1"
+                />
+                <defs>
+                  <linearGradient
+                    id="linear2"
+                    x1="192.553"
+                    y1="28.553"
+                    x2="899.66"
+                    y2="735.66"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stopColor="#fff" />
+                    <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                  </linearGradient>
+                </defs>
+              </svg>
+            </div>
+            <div
+              className="pointer-events-none absolute top-0 right-0 bottom-0 hidden w-1/2 lg:block"
+              aria-hidden="true"
+            >
+              <svg
+                className="absolute inset-0 h-full w-full"
+                width={160}
+                height={678}
+                viewBox="0 0 160 678"
+                fill="none"
+                preserveAspectRatio="xMidYMid slice"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M-161 679.107L546.107-28l707.103 707.107-707.103 707.103L-161 679.107z"
+                  fill="url(#linear3)"
+                  fillOpacity=".1"
+                />
+                <defs>
+                  <linearGradient
+                    id="linear3"
+                    x1="192.553"
+                    y1="325.553"
+                    x2="899.66"
+                    y2="1032.66"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stopColor="#fff" />
+                    <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                  </linearGradient>
+                </defs>
+              </svg>
+            </div>
+            <h1 className="text-4xl font-bold text-white">Contact</h1>
+            <p className="mt-6 max-w-3xl text-base text-white">
+              Societatea noastră îşi desfăşoră activitatea atât în cadrul
+              biroului din Iași cât şi în județele conexe, tocmai pentru a fi
+              cât mai aproape de clienţii noştri şi pentru a veni în
+              întâmpinarea necesităţilor acestora. Ne puteţi contacta folosind
+              informaţiile de mai jos sau formularul de contact.
+            </p>
+            <dl className="mt-8 space-y-6 font-bold text-white">
+              <dt>
+                <span className="sr-only">Adresă</span>
+              </dt>
+              <dd className="flex text-base">
+                <MapPinIcon
+                  className=" h-6 w-6 flex-shrink-0"
+                  aria-hidden="true"
+                />
+                <span className="ml-3">Strada Tătărași 9, Iași, România</span>
+              </dd>
+              <dt>
+                <span className="sr-only">Număr de telefon</span>
+              </dt>
+              <dd className="flex text-base">
+                <PhoneIcon
+                  className=" h-6 w-6 flex-shrink-0"
+                  aria-hidden="true"
+                />
+                <a className="ml-3" href="tel:+40752 171 727">
+                  0752 171 727
+                </a>
+              </dd>
+              <dt>
+                <span className="sr-only">Email</span>
+              </dt>
+              <dd className="flex text-base">
+                <EnvelopeIcon
+                  className=" h-6 w-6 flex-shrink-0"
+                  aria-hidden="true"
+                />
+                <a className="ml-3" href="mailto:contact@mpp.ro">
+                  contact@mpp.ro
+                </a>
+              </dd>
+            </dl>
+            <ul role="list" className="mt-8 flex space-x-12">
+              <li>
+                <a
+                  className="text-white"
+                  href="https://www.facebook.com/mpp.law.ro"
+                >
+                  <span className="sr-only">Facebook</span>
+                  <svg
+                    width={24}
+                    height={24}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    aria-hidden="true"
+                  >
+                    <path
+                      d="M22.258 1H2.242C1.556 1 1 1.556 1 2.242v20.016c0 .686.556 1.242 1.242 1.242h10.776v-8.713h-2.932V11.39h2.932V8.887c0-2.906 1.775-4.489 4.367-4.489 1.242 0 2.31.093 2.62.134v3.037l-1.797.001c-1.41 0-1.683.67-1.683 1.653v2.168h3.362l-.438 3.396h-2.924V23.5h5.733c.686 0 1.242-.556 1.242-1.242V2.242C23.5 1.556 22.944 1 22.258 1"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-white"
+                  href="https://www.facebook.com/mpp.law.ro"
+                >
+                  <span className="sr-only">LinkedIn</span>
+                  <svg
+                    width={24}
+                    height={24}
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    aria-hidden="true"
+                  >
+                    <path
+                      d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24H22.225c.966 0 1.771-.773 1.771-1.729V1.729c0-.956-.805-1.729-1.771-1.729z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Contact form */}
+          <div className="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
+            <h3 className="text-gray-900 text-lg font-medium">
+              Send us a message
+            </h3>
+            <form
+              action="#"
+              method="POST"
+              className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+            >
+              <div>
+                <label
+                  htmlFor="last-name"
+                  className="text-gray-900 block text-sm font-medium"
+                >
+                  Nume si prenume
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    autoComplete="family-name"
+                    className="border-gray-300 text-gray-900 focus:border-indigo-500 focus:ring-indigo-500 block w-full rounded-md py-3 px-4 shadow-sm"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="text-gray-900 block text-sm font-medium"
+                >
+                  Email
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    className="border-gray-300 text-gray-900 focus:border-indigo-500 focus:ring-indigo-500 block w-full rounded-md py-3 px-4 shadow-sm"
+                  />
+                </div>
+              </div>
+              <div>
+                <div className="flex justify-between">
+                  <label
+                    htmlFor="phone"
+                    className="text-gray-900 block text-sm font-medium"
+                  >
+                    Phone
+                  </label>
+                  <span id="phone-optional" className="text-gray-500 text-sm">
+                    Optional
+                  </span>
+                </div>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    name="phone"
+                    id="phone"
+                    autoComplete="tel"
+                    className="border-gray-300 text-gray-900 focus:border-indigo-500 focus:ring-indigo-500 block w-full rounded-md py-3 px-4 shadow-sm"
+                    aria-describedby="phone-optional"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="subject"
+                  className="text-gray-900 block text-sm font-medium"
+                >
+                  Subject
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    name="subject"
+                    id="subject"
+                    className="border-gray-300 text-gray-900 focus:border-indigo-500 focus:ring-indigo-500 block w-full rounded-md py-3 px-4 shadow-sm"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <div className="flex justify-between">
+                  <label
+                    htmlFor="message"
+                    className="text-gray-900 block text-sm font-medium"
+                  >
+                    Message
+                  </label>
+                  <span id="message-max" className="text-gray-500 text-sm">
+                    Max. 500 characters
+                  </span>
+                </div>
+                <div className="mt-1">
+                  <textarea
+                    id="message"
+                    name="message"
+                    rows={4}
+                    className="border-gray-300 text-gray-900 focus:border-indigo-500 focus:ring-indigo-500 block w-full rounded-md py-3 px-4 shadow-sm"
+                    aria-describedby="message-max"
+                    defaultValue={""}
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2 sm:flex sm:justify-end">
+                <button
+                  type="submit"
+                  className="bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 mt-2 inline-flex w-full items-center justify-center rounded-md border border-transparent px-6 py-3 text-base font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 sm:w-auto"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Contact;
