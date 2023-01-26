b53f9766e891a81899f37ffcb3bb8b8931491732
DanCristianIlie <ilie.dancrist@protonmail.com>
2023-01-26 20:15:14 +0200

    fix build errors

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -7,16 +7,32 @@ export const config = {
   },
 };
 
+class MyError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "MyError";
+  }
+}
+
+interface ContactBody {
+  message: {
+    name: string;
+    phone: string;
+    purpose: string;
+    message: string;
+  };
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
     try {
-      await contact(req.body);
+      await contact(req.body as ContactBody);
       res.json({ received: true });
     } catch (err) {
-      res.status(400).send(`Webhook Error: ${(err as any).message}`);
+      res.status(400).send(`Webhook Error: ${(err as MyError).message}`);
       return;
     }
   } else {
